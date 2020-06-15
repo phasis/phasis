a67f62ff736bd8a7e0a1c7f2a82492c3d312aa3d
phasis <info@phasis.it>
2020-06-15 07:48:11 +0200

    Update dbdata.py

--- phasis/dbdata.py
+++ phasis/dbdata.py
@@ -1,3 +1,20 @@
+# -*- coding: iso-8859-1 -*-
+#
+#  Copyright (C) 2001 - 2020 Massimo Gerardi all rights reserved.
+#
+#  Author: Massimo Gerardi massimo.gerardi@gmail.com
+#
+#  Copyright (c) 2020 Qsistemi.com.  All rights reserved.
+#
+#  Viale Giorgio Ribotta, 11 (Roma)
+#  00144 Roma (RM) - Italy
+#  Phone: (+39) 06.87.163
+#  
+#
+#  Si veda file COPYING per le condizioni di software.
+#
+#   www.qsistemi.com - italy@qsistemi.com
+
 
 #ver 0.0.2
 import string
